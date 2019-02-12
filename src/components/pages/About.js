e0e46196aec76c7b5b1c4a35ad67a0de7e632b5f
rubal2611 <rubal2611@gmail.com>
2019-02-12 17:09:25 +1100

    First Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the Todo App</p>
+    </React.Fragment>
+  )
+}
+
+
